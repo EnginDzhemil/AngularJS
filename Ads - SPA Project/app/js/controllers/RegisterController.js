29ec0f37b06cc629dea2d9749a4191a5165a86c8
engindzhemil <edzhemil@gmail.com>
2015-01-08 01:50:38 +0200

    added login/register functionality

---
+++ Ads - SPA Project/app/js/controllers/RegisterController.js
@@ -0,0 +1,20 @@
+'use strict';
+
+app.controller('RegisterController',
+    function ($scope, $location, townsService, authService, notifyService) {
+        $scope.userData = {townId: null};
+        $scope.towns = townsService.getTowns();
+
+        $scope.register = function(userData) {
+            authService.register(userData,
+                function success() {
+                    notifyService.showInfo("Login successful");
+                    $location.path("/");
+                },
+                function error(err) {
+                    notifyService.showError("User registration failed", err);
+                }
+            );
+        };
+    }
+);
