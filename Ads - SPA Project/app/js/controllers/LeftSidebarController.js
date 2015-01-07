c1b6c3b047c1351f15bccafb899cf92e7f968d4c
engindzhemil <edzhemil@gmail.com>
2015-01-07 17:36:14 +0200

    Added base code for controllers and services

---
+++ Ads - SPA Project/app/js/controllers/LeftSidebarController.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// The RightSidebarController controls the content displayed in the right sidebar
+app.controller('LeftSidebarController',
+    function ($scope, categoriesService, townsService) {
+        // TODO
+    }
+);
