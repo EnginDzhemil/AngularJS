7b680ceb8924843c6cf639dd51c3fcc6550dbf28
engindzhemil <edzhemil@gmail.com>
2015-01-08 15:56:37 +0200

    Added angularjs loading bar

--- Ads - SPA Project/app/js/controllers/AppController.js
+++ Ads - SPA Project/app/js/controllers/AppController.js
@@ -5,7 +5,7 @@ app.controller('AppController',
     function ($scope, authService, notifyService, $location) {
         // Put the authService in the $scope to make it accessible from all screens
         $scope.authService = authService;
-
+        $scope.location = $location;
         // Implement the "logout" button click event handler
         $scope.logout = function() {
             authService.logout();
